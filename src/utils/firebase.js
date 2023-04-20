8cb7444b080ca703b2143a41a986103b68b542e7
Unknow <20213tn102@utez.edu.mx>
2023-04-19 23:12:58 -0600

    Cambios firebase

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,1 +1,17 @@
-export * from './firebase';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCizp8p01OC-mbTjqTL7xSPCxSJIc4XSt0",
+  authDomain: "recupera-27d9f.firebaseapp.com",
+  projectId: "recupera-27d9f",
+  storageBucket: "recupera-27d9f.appspot.com",
+  messagingSenderId: "268366413976",
+  appId: "1:268366413976:web:bfa4cb00f7226c1b8a10ed"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
